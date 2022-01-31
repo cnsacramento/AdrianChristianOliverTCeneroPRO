4d24264f54f865d9b1e31f7cfffbe2e043f2df47
Christian Novo Sacramento <christianivh366@gmail.com>
2022-01-31 13:14:50 +0000

    Creado el método generarAleatorio(clase Partida)

--- AdivinarNumeros/src/adivinarnumeros/Partida.java
+++ AdivinarNumeros/src/adivinarnumeros/Partida.java
@@ -4,6 +4,8 @@
  */
 package adivinarnumeros;
 
+import java.util.Random;
+
 /**
  *
  * @author Christian Novo Sacramento
@@ -14,9 +16,44 @@ public class Partida {
     private int dificultad;
     private String jugador;
     
-    Partida(int dificultad, String jugador) {
+    
+    /**
+     * 
+     * @param dificultad grado de dificultad de la partida elegida por el jugador
+     * @param jugador persona que juega la partida
+     */
+    Partida (int dificultad, String jugador) {
         
         this.dificultad = dificultad;
         this.jugador = jugador;
+    }
+    
+    /**
+     * Método que sirve para generar un número aleatorio en función de la dificultad
+     * @return devuelve el número aleatorio llamado número secreto
+     */
+    public int generarAleatorio() {
+        
+        Random rnd = new Random();
+        switch (dificultad) {
+            //dificulad 1 = fácil
+            case 1:
+                numeroSecreto = rnd.nextInt(10);
+                break;
+            //dificultad 2 = media
+            case 2:
+                numeroSecreto = rnd.nextInt(100);
+                break;
+            //dificultad 3 = díficil
+            case 3:
+                numeroSecreto = rnd.nextInt(1000);
+                break;
+            //dificultad 4 = Leyenda
+            case 4:
+                numeroSecreto = rnd.nextInt(10000);
+                break;                
+        }
+        
+        return numeroSecreto;
     }
 }
