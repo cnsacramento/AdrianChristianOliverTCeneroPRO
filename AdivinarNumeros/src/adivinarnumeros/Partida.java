b3b9bbe5e8d88a9cf906413dc3e7d47a2a7caa3e
Christian Novo Sacramento <christianivh366@gmail.com>
2022-01-31 12:57:04 +0000

    Creada clase partida con atributos y un constructor

---
+++ AdivinarNumeros/src/adivinarnumeros/Partida.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adivinarnumeros;
+
+/**
+ *
+ * @author Christian Novo Sacramento
+ */
+public class Partida {
+    
+    private int numeroSecreto;
+    private int dificultad;
+    private String jugador;
+    
+    Partida(int dificultad, String jugador) {
+        
+        this.dificultad = dificultad;
+        this.jugador = jugador;
+    }
+}
