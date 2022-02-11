302f0756d270212532c778b63d84bc21f7ad70fa
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-11 01:05:18 +0000

    Se añaden atributos a la clase Proyecto

---
+++ Bitacora/src/bitacora/Proyecto.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bitacora;
+
+/**
+ *
+ * @author Christian Novo Sacramento
+ */
+public class Proyecto {
+    
+    private String nombre;
+    private String descripcion;
+}
