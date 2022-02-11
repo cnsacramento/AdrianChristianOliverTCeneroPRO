a587e126e4b4e586853d69011500ce298653ed40
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-11 02:44:20 +0000

    Se añade Getters y Setters, atributo HashMap y constructores

--- Bitacora/src/bitacora/Proyecto.java
+++ Bitacora/src/bitacora/Proyecto.java
@@ -4,6 +4,8 @@
  */
 package bitacora;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Christian Novo Sacramento
@@ -12,4 +14,45 @@ public class Proyecto {
     
     private String nombre;
     private String descripcion;
+    private HashMap<String,Anotacion> anotaciones;
+    
+    /**
+     * Contructor vacío del Proyecto
+     */
+    public Proyecto(){}
+    
+    public Proyecto(String nombre, String descripcion, HashMap anotaciones) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.anotaciones = anotaciones;
+    }
+    
+    public String toString() {
+        return "El nombre del proyecto es: " + nombre 
+                +"\nY su descripción es: " + descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public HashMap<String, Anotacion> getAnotaciones() {
+        return anotaciones;
+    }
+
+    public void setAnotaciones(HashMap<String, Anotacion> anotaciones) {
+        this.anotaciones = anotaciones;
+    }
 }
