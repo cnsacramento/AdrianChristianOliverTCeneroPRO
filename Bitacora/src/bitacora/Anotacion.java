1375095dfc2a82e5dcce4249361625145b9f9517
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-11 02:41:17 +0000

    Se documenta la clase Anotacion

--- Bitacora/src/bitacora/Anotacion.java
+++ Bitacora/src/bitacora/Anotacion.java
@@ -16,18 +16,30 @@ public class Anotacion {
     private String cuerpo;
     private Date date;
     
+    /**
+     * Constructor que otorga valores por defecto
+     */
     public Anotacion() {
         cabecera = null;
         cuerpo = null;
     }
     
+    /**
+     * Contructor que rellena la anotación
+     * @param cabecera mensaje descriptivo anotación
+     * @param cuerpo informe completo
+     */
     public Anotacion(String cabecera, String cuerpo) {
         this.cabecera = cabecera;
         this.cuerpo = cuerpo;
     }
     
+    /**
+     * Método que devuelve un String al main
+     * @return el contenido de la anotación
+     */
     public String toString() {
         
-        return "\t" + cabecera + "\n" + cuerpo;
+        return "\t " + cabecera + "\n" + cuerpo;
     }
 }
