fcbe93ce681afa4cc45946fcf802e2523a2c332c
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-13 12:10:07 +0000

    Se añade método para añadir los proyectos, se modifica el constructor y se realizan el comentado

--- Bitacora/src/bitacora/GestorProyectos.java
+++ Bitacora/src/bitacora/GestorProyectos.java
@@ -14,12 +14,25 @@ public class GestorProyectos {
     
     private HashMap<String,Proyecto> proyectos;
     
-    public GestorProyectos(){} //Constructor sin parámetros
+    /**
+     * Constructor que crean un hasmap vacío
+     */
+    public GestorProyectos(){
+        this.proyectos = new HashMap<>();
+    } 
 
     public GestorProyectos(HashMap<String, Proyecto> proyectos) {
         this.proyectos = proyectos;
     }
-    
+    /**
+     * Método que sirve para añadir proyectos
+     * @param nombre forma la clave del hasmap de proyectos
+     * @param proyecto recibe el objeto proyecto
+     */
+    public void anyadirProyecto(String nombre, Proyecto proyecto) {
+        this.proyectos.put(nombre, proyecto);
+    }
+
 
     public HashMap<String, Proyecto> getProyectos() {
         return proyectos;
