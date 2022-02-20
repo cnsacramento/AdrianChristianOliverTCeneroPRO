7553a169aca8caa394e1e5c6369cdf5323908993
Christian Novo Sacramento <christianivh366@gmail.com>
2022-02-20 23:08:09 +0000

    Se añade la ordenacion de los proyectos por fechas

--- Bitacora/src/bitacora/GestorProyectos.java
+++ Bitacora/src/bitacora/GestorProyectos.java
@@ -4,7 +4,11 @@
  */
 package bitacora;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -13,12 +17,14 @@ import java.util.HashMap;
 public class GestorProyectos {
     
     private HashMap<String,Proyecto> proyectos;
-    
+    private ArrayList<Map.Entry<String,Proyecto>> proyectoOrdenados; 
+     
     /**
      * Constructor que crean un hasmap vacío
      */
     public GestorProyectos(){
         this.proyectos = new HashMap<>();
+        this.proyectoOrdenados = new ArrayList<>(proyectos.entrySet());
     } 
 
     public GestorProyectos(HashMap<String, Proyecto> proyectos) {
@@ -32,7 +38,29 @@ public class GestorProyectos {
     public void anyadirProyecto(String nombre, Proyecto proyecto) {
         this.proyectos.put(nombre, proyecto);
     }
+    
+    /**
+     * Método que ordena los proyectos por fechas
+     */
+    public ArrayList ordenarProyectos () {
+        proyectoOrdenados = new ArrayList<>(proyectos.entrySet());
+        Collections.sort(proyectoOrdenados,new Comparator<Map.Entry<String, Proyecto>>() {
+            @Override
+            public int compare(Map.Entry<String, Proyecto> pro1, Map.Entry<String, Proyecto> pro2) {
+                return pro1.getValue().getFecha().compareTo(pro2.getValue().getFecha());
+            }
+        });
+        
+        return proyectoOrdenados;
+    }
 
+    public ArrayList<Map.Entry<String, Proyecto>> getProyectoOrdenados() {
+        return proyectoOrdenados;
+    }
+
+    public void setProyectoOrdenados(ArrayList<Map.Entry<String, Proyecto>> proyectoOrdenados) {
+        this.proyectoOrdenados = proyectoOrdenados;
+    }
 
     public HashMap<String, Proyecto> getProyectos() {
         return proyectos;
